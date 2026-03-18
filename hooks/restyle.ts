9ddf98827b83749324cae3737c590577e006c4e0
Alexander Fedoriako <139990833+i-am-a-saw@users.noreply.github.com>
2026-03-19 00:43:53 +0300

    feat: added hooks for restyle theme

---
+++ hooks/restyle.ts
@@ -0,0 +1,6 @@
+import { createBox, createText, useTheme } from "@shopify/restyle";
+import type { AppTheme } from "@/constants/restyle-theme";
+
+export const Box = createBox<AppTheme>();
+export const Text = createText<AppTheme>();
+export const useAppTheme = () => useTheme<AppTheme>();
